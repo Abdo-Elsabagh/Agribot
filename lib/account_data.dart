e559637e4cc1d2ff6fd1e60f81787020f80c37f2
Abdo-Elsabagh <aelsabaggha2000@gmail.com>
2023-12-04 00:53:36 +0200

    add profile

--- lib/account_data.dart
+++ lib/account_data.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/login_view.dart';
 
 class Account extends StatelessWidget {
   const Account({super.key});
@@ -6,12 +7,103 @@ class Account extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[400],
       appBar: AppBar(
+        backgroundColor: Colors.grey[400],
+        elevation: 0,
         centerTitle: true,
         title: const Text(
           'My Profile',
           style: TextStyle(fontSize: 30, fontWeight: FontWeight.w900),
         ),
+      ),
+      body: Center(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(top: 15.0),
+                child: Center(
+                  child: CircleAvatar(
+                    radius: 70,
+                    backgroundColor: Colors.white70,
+                    child: Icon(
+                      Icons.account_circle,
+                      size: 110,
+                    ),
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(top: 20.0, left: 40),
+                child: Text(
+                  'Name',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 30.0),
+                child: Container(
+                  alignment: Alignment.centerLeft,
+                  height: 50,
+                  width: 320,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.white),
+                  child: const Padding(
+                    padding: EdgeInsets.only(left: 8.0),
+                    child: Icon(Icons.account_circle),
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(top: 20.0, left: 40),
+                child: Text(
+                  'Email',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+                ),
+              ),
+              Padding(
+                  padding: const EdgeInsets.only(left: 30.0),
+                  child: Container(
+                    alignment: Alignment.centerLeft,
+                    height: 50,
+                    width: 320,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.white),
+                    child: const Padding(
+                      padding: EdgeInsets.only(left: 8.0),
+                      child: Icon(Icons.email),
+                    ),
+                  )),
+              const SizedBox(
+                height: 20,
+              ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.of(context).pushReplacement(MaterialPageRoute(
+                    builder: (context) => const LoginViem(),
+                  ));
+                },
+                child: Center(
+                  child: Container(
+                    width: 200,
+                    alignment: Alignment.center,
+                    height: 40,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: Colors.blue),
+                    child: const Text(
+                      'LOG OUT',
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              ),
+            ]),
       ),
     );
   }
